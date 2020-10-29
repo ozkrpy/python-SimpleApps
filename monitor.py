6ef656eaf649bc665683d42d1361ad715e78811a
ozkrpy <ozkrpy@gmail.com>
2020-10-29 16:52:21 -0300

    separacion en funciones para insertar y leer desde la bd

--- monitor.py
+++ monitor.py
@@ -5,23 +5,64 @@ import datetime
 import time
 import csv
 import psycopg2
+import cv2
 
-
-s = speedtest.Speedtest()
 conn = psycopg2.connect(
-    host="pg12-pre",
-    database="monitor",
-    user="dbadmin",
-    password="areguapgpre")
-try:
+			host="pg12-pre",
+			database="monitor",
+			user="dbadmin",
+			password="areguapgpre")
+
+
+def insertar_db(bajada: int, subida: int):
+	global conn 
+	# global cur
+	try:
+		cur = conn.cursor()
+		sql = 'insert into toma_muestras (velocidad_subida, velocidad_bajada) values (%s, %s);'
+		cur.execute(sql, (bajada,subida,))
+		# cur.execute()
+		conn.commit()
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+	# finally:
+	# 	if conn is not None:
+	# 		conn.close()
+	# 		print('Database connection closed.')
+
+def consulta_db():
+	global conn 
+	# global cur
+	try:
+		cur = conn.cursor()
+		sql = 'select max(fecha_registro) from public.toma_muestras;'
+		cur.execute(sql)
+		ultimo = cur.fetchone()
+		# cur.execute()
+		# print(ultimo)
+		
+		return ultimo[0].strftime("%b %d %Y %H:%M:%S")
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+
+def tomar_muestra():
+	s = speedtest.Speedtest()
 	downspeed = s.download()
 	upspeed = s.upload()
-
-	cur = conn.cursor()
+	insertar_db(downspeed, upspeed)
 	
-	cur.execute('insert into toma_muestras (velocidad_subida, velocidad_bajada) values (%s);', (downspeed,upspeed))
-	# cur.execute()
-	conn.commit()
+try:
+	while True:
+		k = cv2.waitKey(1) & 0xFF
+		# press 'q' to exit
+		if k == ord('q'):
+			conn.close()
+			break
+		tomar_muestra()
+		print ('ultimo registro insertado:', consulta_db())
+		time.sleep(60)
+except (Exception, psycopg2.DatabaseError) as error:
+	print(error)
 	#print(db_version)
 	
 	# with open('test.csv', mode='w') as speedcsv:
@@ -38,10 +79,5 @@ try:
 	# 			'bajada': upspeed
 	# 		})
 	#time.sleep(60)
-except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-finally:
-	if conn is not None:
-        	conn.close()
-        	print('Database connection closed.')
+
 	
